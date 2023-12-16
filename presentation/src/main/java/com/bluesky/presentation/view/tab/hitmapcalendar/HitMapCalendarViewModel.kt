557bb335b358aadde367c9ec241951506cc03718
BlueSky <dbcjdrb1997@gmail.com>
2023-12-17 00:08:56 +0900

    ErrorCode. 수정 (GetSettingUseCase)

--- presentation/src/main/java/com/bluesky/presentation/view/tab/hitmapcalendar/HitMapCalendarViewModel.kt
+++ presentation/src/main/java/com/bluesky/presentation/view/tab/hitmapcalendar/HitMapCalendarViewModel.kt
@@ -1,7 +1,6 @@
 package com.bluesky.presentation.view.tab.hitmapcalendar
 
 import androidx.lifecycle.ViewModel
-import com.bluesky.domain.usecase.setting.GetSettingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
