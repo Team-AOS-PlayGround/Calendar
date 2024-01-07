1f9ea81d3b9ca6b125d1f633cae092e6cfda19d9
pachuho <juho7410@gmail.com>
2024-01-07 16:56:44 +0900

    [Presentation] MainViewModel 내부 Calendar UseCase 구성

--- presentation/src/main/java/com/bluesky/presentation/view/main/MainViewModel.kt
+++ presentation/src/main/java/com/bluesky/presentation/view/main/MainViewModel.kt
@@ -2,7 +2,9 @@ package com.bluesky.presentation.view.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.bluesky.domain.usecase.calendar.GetCalendarUseCase
 import com.bluesky.domain.usecase.setting.GetSettingUseCase
+import com.kizitonwose.calendar.core.CalendarDay
 //import com.bluesky.domain.usecase.setting.GetSettingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -10,11 +12,17 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val getSettingUseCase: GetSettingUseCase
+    private val getSettingUseCase: GetSettingUseCase,
+    private val getCalendarUseCase: GetCalendarUseCase
 ) : ViewModel() {
-   val isDarkTheme = getSettingUseCase.getIsDarkTheme()
+    val isDarkTheme = getSettingUseCase.getIsDarkTheme()
+    val isSelectedDays = getCalendarUseCase.getSelectedDays()
 
     fun updateIsDarkTheme(isDarkTheme: Boolean) = viewModelScope.launch {
         getSettingUseCase.updateIsDarkTheme(isDarkTheme)
+    }
+
+    fun updateIsSelectedDays(isSelectedDays: List<CalendarDay>) = viewModelScope.launch {
+        getCalendarUseCase.updateIsDays(isSelectedDays)
     }
 }
